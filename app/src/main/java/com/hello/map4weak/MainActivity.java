0db568b192a70d61848bc964dfe55de7464a4e05
Jiin Kim <rlawldls1502@gmail.com>
2022-05-21 15:19:47 +0900

    first commit

---
+++ app/src/main/java/com/hello/map4weak/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hello.map4weak;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
