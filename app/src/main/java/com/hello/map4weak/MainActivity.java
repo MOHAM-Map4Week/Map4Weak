7e6be6d89b26451b97eaa5f39aea53c8a52aa97e
yewon0804 <ehdnjs0804@gmail.com>
2022-05-28 19:41:05 +0900

    Map test.. 중심점 변경, 마커 추가
    중심점을 충북대학교 위치로 변경
    충북대학교 위치에 마커 샘플 추가

--- app/src/main/java/com/hello/map4weak/MainActivity.java
+++ app/src/main/java/com/hello/map4weak/MainActivity.java
@@ -5,6 +5,9 @@ import android.view.ViewGroup;
 
 //추가한 부분 1
 import androidx.appcompat.app.AppCompatActivity;
+
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,7 +18,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         MapView mapView = new MapView(this);
+        MapPoint CBNU = MapPoint.mapPointWithGeoCoord(36.628986, 127.456355);
+
+        // 중심점 변경
+        mapView.setMapCenterPoint(CBNU, true);
+
         ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
+
+        MapPOIItem marker = new MapPOIItem();
+        marker.setItemName("충북대학교");
+        marker.setTag(0);
+        marker.setMapPoint(CBNU);
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+
+        mapView.addPOIItem(marker);
     }
 }
