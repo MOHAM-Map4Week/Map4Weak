89f380b213562c0acf4df72f0a3e94fbe69a953f
Jiin Kim <rlawldls1502@gmail.com>
2022-06-08 15:03:12 +0900

    후기 페이지 내용 생성

--- app/src/main/java/com/hello/map4weak/Review.java
+++ app/src/main/java/com/hello/map4weak/Review.java
@@ -2,7 +2,10 @@ package com.hello.map4weak;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Review extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class Review extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
+
+        TextView movestar = findViewById(R.id.star);
+
+        TextView movepencil = findViewById(R.id.pencil);
+
+        movestar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),Review.class);
+                startActivity(intent);
+            }
+        });
+
+        movepencil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), mypage.class);
+            }
+        });
     }
 }
