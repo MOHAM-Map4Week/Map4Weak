d9ac257c8583b689529df3d4184701a3edb21fd1
Jiin Kim <rlawldls1502@gmail.com>
2022-06-11 05:03:13 +0900

    게시글목록 확인

--- app/src/main/java/com/hello/map4weak/Update.java
+++ app/src/main/java/com/hello/map4weak/Update.java
@@ -1,8 +1,16 @@
 package com.hello.map4weak;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Update extends AppCompatActivity {
 
@@ -10,5 +18,84 @@ public class Update extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
+
+        TextView movetext = findViewById(R.id.announce);
+
+        ImageView movestar = findViewById(R.id.star);
+
+        ImageView movepencil = findViewById(R.id.pencil);
+
+        ImageView movesetting = findViewById(R.id.setting);
+
+        ImageView back = findViewById(R.id.goback);
+
+        Button delete = findViewById(R.id.delete);
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(Update.this);
+                builder.setTitle("삭제하기");
+                builder.setMessage("이 글을 삭제하시겠습니까?");
+                builder.setPositiveButton("예",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(getApplicationContext(),"삭제 완료",Toast.LENGTH_LONG).show();
+                                dialog.cancel();
+                                Intent intent = new Intent(getApplicationContext(), mypage.class);
+                                startActivity(intent);
+                            }
+                        });
+                builder.setNegativeButton("아니오",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(getApplicationContext(),"취소",Toast.LENGTH_LONG).show();
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alertD = builder.create();
+                alertD.show();
+
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        movetext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),TipOff.class);
+                startActivity(intent);
+            }
+        });
+
+        movestar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),Review.class);
+                startActivity(intent);
+            }
+        });
+
+        movepencil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), mypage.class);
+            }
+        });
+
+        movesetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),setting.class);
+                startActivity(intent);
+            }
+        });
     }
 }
